16b9940ce3a20bc766c4eb822c36071aac75724b
Reswara26 <ganendra.rashi96@gmail.com>
2025-09-16 11:39:01 +0800

    Add files via upload

---
+++ model/Armada.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+
+public class Armada {
+    private String id;
+    private String tipe;
+    private int kapasitas;
+
+    public Armada(String id, String tipe, int kapasitas) {
+        this.id = id;
+        this.tipe = tipe;
+        this.kapasitas = kapasitas;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+}
